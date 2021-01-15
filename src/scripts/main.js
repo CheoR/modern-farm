bc87efe0384fcb53910de078d34857f1fad4802f
CheoR <cheo.roman+github@gmail.com>
2021-01-15 17:47:46 -0600

    Add seed objects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,22 @@
 console.log("Welcome to the main module")
 
+import { createPlan } from "./plan.js"
+
+const yearlyPlan = createPlan();
+
+console.log("Modern Farm Management")
+console.table(yearlyPlan);
+
+import { createCorn } from "./seeds/createCorn.js"
+console.log("Test Your Logics")
+let corn = createCorn();
+console.log("\n");
+console.log(corn);
+console.log("\n");
+import  { createPotato } from "./seeds/createPotato.js"
+
+let potato = createPotato()
+
+console.log(potato)
+
+
